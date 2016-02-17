d1093c580a6c9d3ee76133dd7f2334e2acc2cd73
yellowkui <huangkui198707@qq.com>
2016-02-17 17:27:52 +0800

    160217 for

--- python/if.py
+++ python/if.py
@@ -9,28 +9,45 @@
 
 
 #if语句的使用 2016-02-04
-"""
-a = input();
 
-if(int(a)== 100):
-    print('你输入的数字相同');
-elif(int(a)>100):
-    print('你输入的数字['+a+']大于100!');
-else:
-    print('你输入的数字['+a+']小于100!');
+#a = input();
 
-"""
-
-a = input();
-
-if(int(a)== 100):
-    print('你输入的数字相同');
-elif(int(a)>100):
-    print('你输入的数字['+a+']大于100!');
-else:
-    print('你输入的数字['+a+']小于100!');
+#if(int(a)== 100):
+#    print('你输入的数字相同');
+#elif(int(a)>100):
+#    print('你输入的数字['+a+']大于100!');
+#else:
+#    print('你输入的数字['+a+']小于100!')
 
 
+#########################
+#体重BIM判断
+##########################
+height = float(input(r"请输入你的身高，比如170，输入‘1.70’"+"\n"))
+weight = float(input(r"请输入你的体重，比例如70KG，输入70"+"\n"))
+
+bmi = weight/(height**2)
+print("你的BMI指数是%.3f" % bmi)
+if(bmi<=18.5):
+    print("你的属于‘过轻’")
+elif(bmi<=25):
+    print("你的属于‘正常’")
+elif(bmi<=28):
+    print("你的属于‘过重’")
+elif(bmi<=32):
+    print("你的属于‘肥胖’")
+elif(bmi>32):
+    print("你的属于‘严重肥胖’")
+    pass
+
+
+
+
+    1
+    2
+    3
+    4
+    44
 
 
 
