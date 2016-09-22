71d760ee0f893c9b8c2c11d54d25194d392ce3b2
yellowkui <huangkui198707@qq.com>
2016-09-22 16:06:32 +0800

    adf

--- python/10highfun.py
+++ python/10highfun.py
@@ -78,22 +78,94 @@
 #
 #print(str2float('1234.5634'))
 
+#
+#from functools import reduce
+#
+#
+#def str2num(str):
+#        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[str]
+#
+#def f1(x,y):
+#        return x * 10 + y
+#
+#s = '1234.456'
+#
+##print(map(str2num,s[0]))
+#
+#print(reduce(f1,map(str2num,s[0])))
+#
+#
+##  return reduce(f1,map(str2num,s[0])) + reduce(f2,list(map(str2num,s[1]))[::-1])/10
+#
 
-from functools import reduce
 
 
-def str2num(str):
-        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[str]
+###通过 filter函数 ，生成所有素数
+#
+#def createJS():##创建奇数
+#    n = 1
+#    while True:
+#        n = n + 2
+#        yield n
+#
+#def ter_u(x):     ##创建一个过滤器
+#    for i in range(x):
+#        return x % i != 0
+#
+#for i in createJS():
+#    if i > 100:
+#        break
+#    print(filter(ter_u,i))
+#
+#
 
-def f1(x,y):
-        return x * 10 + y
-
-s = '1234.456'
-
-#print(map(str2num,s[0]))
-
-print(reduce(f1,map(str2num,s[0])))
 
 
-#  return reduce(f1,map(str2num,s[0])) + reduce(f2,list(map(str2num,s[1]))[::-1])/10
+
+
+
+
+####   装饰器  2016-09-18
+####装饰器必须定义在被装饰函数前
+
+def log(func):                      
+    def apper(*args , **kw):
+        print('call:%s '% func.__name__)
+        return(func(*args,**kw))
+    return apper
+
+@log                 ### 关键语句进行装饰器定义
+def printnow():
+    print('2016-01-01')
+
+printnow()
+
+
+
+
+####   装饰器 带函数  2016-09-18
+####装饰器必须定义在被装饰函数前
+
+
+def log_()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
