d7973e692f002abd48f6da50e9999af179f6e3a2
kui_home <huangkui198707@qq.com>
2017-11-22 23:05:36 +0800

    1122

--- python/11oop.py
+++ python/11oop.py
@@ -14,11 +14,14 @@
 #
 #
 #
-#lilei = student('lilei',123)
-#pandd = student('pandd',22)
+##lilei = student('lilei',123)
+##pandd = student('pandd',22)
 #
-
-
+#
+#kui = student('kui',22)
+#kui.sex = 1
+#kui.printA()
+#
 
 #print(student)
 #student
@@ -45,36 +48,141 @@
 #############################
 
 #类的访问控制,在类的属性定义时，定义为私有变量，外部就无法对变量进行修改了，保障程序健壮性
+# 通过_类名__变量，也可以外部访问变量，但是一般不建议这么处理去访问。
+
 
 ################################
+#
+#class Dog(object):
+#    def __init__(self,name = 'Maly',leg = 0,):
+#        self._Dog__leg = 11
+#        self.__name = name
+#        self.__leg = leg
+#
+#    def getName(self):
+#        return self.__name
+#
+#    def getLeg(self):
+#        return self.__leg
+#
+#    def setName(self,str):
+#        self.__name = str
+#
+#    def setLeg(self,x):
+#        self.__leg = x
+#
+#
+#
+#xiaot = Dog('xiaot',4)
+#
+#
+#print(xiaot._Dog__leg)
+#print(xiaot.getName())
+#print(xiaot.getLeg())
+#
 
-class Dog(object):
-    def __init__(self,name = 'Maly',leg = 0,):xiaot._Dog__leg = 11
-        self.__name = name
-        self.__leg = leg
-
-    def getName(self):
-        return self.__name
-
-    def getLeg(self):
-        return self.__leg
-
-    def setName(self,str):
-        self.__name = str
-
-    def setLeg(self,x):
-        self.__leg = x
 
 
+#### 继承 ######
+#
+#class Animal(object):
+#    def __init__(self):
+#        return
+#
+#    def call(self):
+#        print('I am Animal')
+#
+#class Pig(Animal):
+#    def call(self):
+#        print('i am pig')
+#
+#    def eat(self):
+#        print('pig can eat')
+#
+#class Cat(Animal):
+#    pass
+#
+#a = Pig()
+#b = Cat()
+#
+#a.call()
+#a.eat()
+#b.call()
+##
+##
+####通过 dir（）函数可以获取对象的所有方法和属性
+#
+#print(dir(a))
+#
 
-xiaot = Dog('xiaot',4)
-xiaot.setLeg(6)
-
-xiaot._Dog__leg = 11
-print(xiaot.getName())
-print(xiaot.getLeg())
+#import System
 
 
 
 
-##### 继承 ######
+### 面向对象高级编程
+### 1 为实例添加方法，为类增加方法。
+
+class MyObject(object):
+    pass
+
+a = MyObject()
+
+def printname():
+    print('print name')
+
+
+def printscore(self):
+    print('print score')
+
+
+### 第一种方式，给对象进行添加方法
+a.u_printname = printname
+
+a.u_printname()
+
+
+### 第二中方式 ，给类添加方法
+MyObject.printscore = printscore
+
+b = MyObject()
+
+b.printscore()
+
+
+
+##### 限制类的属性  __slots__
+
+#
+#class Student(object):
+#    __slots__ = ('x','y')    # 这里注意是字符
+#    def __init__(self):
+#        self.x = 'default'
+#
+#a = Student()
+#print(a.x)
+#a.x = 'new value'
+#print(a.x)
+##a.name = 'dfsdfsdf'##    由于限制了这一句会报错 
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
