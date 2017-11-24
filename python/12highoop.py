9d1214258a4ee4f186c718722e3b023d944e992d
kui_home <huangkui198707@qq.com>
2017-11-24 23:23:48 +0800

    pc1124 night

--- python/12highoop.py
+++ python/12highoop.py
@@ -75,28 +75,65 @@
 
 
 ####定义类的说明与名称   __str__是给使用者看的， __rptr__是给开发人员看的。
+#
+#class Stu(object):
+#    def __init__(self,value):
+#        self.name = value
+#    def __str__(self):
+#        return 'CLASS Stu'
+#    def __rptr__(self):
+#        return 'Class stu stu'
+#    
+#    #  为了节省时间，可以将__str__直接复制给__rptr__ 
+#    #  __rptr__ = __str__
+#
+#
+#print(dir(Stu))
+#u = Stu('abc')
+#print(u)
+#u
+#
 
-class Stu(object):
-    def __init__(self,value):
-        self.name = value
-    def __str__(self):
-        return 'CLASS Stu'
-    def __rptr__(self):
-        return 'Class stu stu'
-    
-    #  为了节省时间，可以将__str__直接复制给__rptr__ 
-    #  __rptr__ = __str__
+###  __iter__的使用  实现斐波那契数列
+###   实现时循环会将类作为迭代对象 ，不停调用本身，直到运行了StopIteration()
+#class Fib(object):
+#    def __init__(self):
+#        self.a,self.b = 0,1
+#
+#    def __iter__(self):
+#        return selfStopIteration
+#
+#    def __next__(self):
+#        self.a,self.b = self.b ,self.a + self.b
+#        if self.a >= 10000:
+#            raise StopIteration()
+#        return self.a
+#
+#for n in Fib():
+#    print(n)
+#
+#
 
 
-print(dir(Stu))
-u = Stu('abc')
-print(u)
-u
 
+#### __getitem__的使用  使用__iter__不能实现进行具体的位置调用 比如 Fib()[5]
+class Fib(object):
+    def __init__(self):
+        self.a,self.b = 1,2
 
+    def __getitem__(self,value):
+        a ,b = 0,1
+        for n in range(value):
+            a,b = b,a+b
+        return a
 
+print(Fib()[8])
 
-
+#for a in Fib():
+#    print(a)
+#    if a>100:
+#        raise StopIteration()
+#
 
 
 
