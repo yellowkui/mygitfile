d28637364b189c8d317d766019ee8eb156b83826
kui_pc <huangkui198707@qq.com>
2018-07-29 23:43:52 +0800

    e

--- python/spider/04doub.py
+++ python/spider/04doub.py
@@ -13,6 +13,21 @@ from bs4 import BeautifulSoup
 from contextlib import closing
 
 
+class movie(object):
+    def __init__(self):
+        self.moviename = '' #电影名
+        self.daoy = ''   # 导演
+        self.zhuy = ''    # 主演
+        self.bianj  = ''    # 编剧
+        self.jianjie  = ''    # 简介
+
+    def printall(self):
+        print('电影名称: '+self.moviename)
+        print('导演：'+self.daoy)
+        print('主演: '+self.zhuy)
+        print('编剧: '+self.bianj)
+        print('简介：\n '+self.jianjie)
+
 class get_douban(object):
 
     def __init__(self):
@@ -23,9 +38,11 @@ class get_douban(object):
                 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'}
         self.target = 'https://movie.douban.com/top250'
         self.movieUrls = []
+        self.movieid = []
 
 
-    def getMovieUrl(self):
+
+    def getMovieUrl(self):   #获取电影地址,不传参数
         req = requests.get(url = self.target ,headers =  self.headers)
 
         bs = BeautifulSoup(req.text)
@@ -34,18 +51,131 @@ class get_douban(object):
         bs2 = BeautifulSoup(str(bf))
         bf2 = bs2.find_all('a')
         for i in bf2:
-            self.movieUrls.append(i.get('href'))
-        print(self.movieUrls)
+            s = i.get('href')
+            self.movieUrls.append(s)
+            self.movieid.append(s.split('/')[-2])
+      #  print(self.movieUrls)
+
+
+
+    def getMovieUrl(self,path_num):
+        #豆瓣不传参数时，默认为第一页
+        #
+        start_num = path_num * 25  ##豆瓣top250，默认一页有25部电影
+        data = {'start':start_num,'filter':''}    # filter 是处理过滤条件的,现在没用
+        req = requests.get(url = self.target ,headers =  self.headers,params = data)
+
+        bs = BeautifulSoup(req.text)
+        bf = bs.find_all('div',class_ = 'hd')
+
+        bs2 = BeautifulSoup(str(bf))
+        bf2 = bs2.find_all('a')
+        for i in bf2:
+            s = i.get('href')
+            self.movieUrls.append(s)
+            self.movieid.append(s.split('/')[-2])
+        time.sleep(2)  ## 休息下
+
+
+
+      #  print(self.movieUrls)
 
 
 
 
+
+    def saveurls(self,filepath,filename,text):
+        with open(filepath+filename,'wb') as f:       ###追加
+            en = (','.join(text)).encode('utf-8')
+            f.write(en)
+            print('存入完成')
+
+
+
+    def loadurls(self,filepath,filename):
+        with open(filepath+filename,'r') as f:
+            self.movieUrls = f.read().split(',')
+        self.movieid = [s.split('/')[-2] for s in o.movieUrls]    ###通过列表生成式完成列表处理
+        print('读取电影网页地址，总共 %s 条记录' % len(self.movieUrls))
+
+
+
+
+
+
+    def getMovInfo(self,curl,movie):     #获取电影信息
+        s = ''
+        req = requests.get(url = curl,headers =  self.headers)
+        bs = BeautifulSoup(req.text)
+
+        # 01 获取电影名
+        movieName = (bs.find_all('span',property="v:itemreviewed")[0].string) #电影名
+
+        #03 获取简介
+        html_div_span_jj =  bs.find('span',property="v:summary")
+        jianjie = ''
+
+
+        if html_div_span_jj.string is None:
+            for s in html_div_span_jj.strings:
+                jianjie = jianjie+s.strip()
+        else:
+            jianjie = html_div_span_jj.string
+
+#        print(str(jianjie).replace('<br/>','\r'))
+
+        # 02 获取电影信息
+        html_div_info =  bs.find_all('div',id="info")  #0导演, 1编剧，2主演
+        bs2 =  BeautifulSoup(str(html_div_info))
+        html_div_info_span = bs2.find_all('span',class_ = 'attrs')
+
+        info_list = []
+
+        for i in html_div_info_span:
+            tmp_str = ''
+            for j in i.children:
+                tmp_str += str(j.string).strip()
+            info_list.append(tmp_str)
+
+
+
+
+        #end 给传入对象赋值
+        movie.moviename = movieName
+        movie.daoy = info_list[0]       #导演
+        movie.zhuy =  info_list[1]      #主演
+        movie.bianj = info_list[2]       #编剧
+        movie.jianjie = jianjie
 
 
 
 if __name__ == '__main__':
+    
     o = get_douban()
-    o.getMovieUrl()
+    i = 0
+
+
+#for i in range(10):  #由于top250 总共也就10页
+#        print('读取第%s页。' % i )
+#        o.getMovieUrl(i)
+#    o.saveurls('e:\\','doubanmovieurls.txt',o.movieUrls)
+
+
+
+    o.loadurls('e:\\','doubanmovieurls.txt')
+    print(o.movieUrls)
+    print(o.movieid)
+
+#    i = 0
+#    for i in range(10):
+#        m = movie()
+#        o.getMovInfo(o.movieUrls[i],m)
+#        m.printall()
+#        print('\r\r')
+#        time.sleep(2)
+        
+
+
 
 
 
@@ -55,6 +185,7 @@ if __name__ == '__main__':
 
 
     
+
 
 
 
