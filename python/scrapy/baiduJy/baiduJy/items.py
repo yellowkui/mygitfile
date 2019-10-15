c0a5c124a3fd4b469a649476ac745e21f4d64a2f
huangkui <huangkui198707@qq.com>
2019-10-15 17:43:17 +0800

    baiduyj2

--- python/scrapy/baiduJy/baiduJy/items.py
+++ python/scrapy/baiduJy/baiduJy/items.py
@@ -11,4 +11,9 @@ import scrapy
 class BaidujyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+
+
+    name = scrapy.Field()
+    djsum = scrapy.Field()
+
     pass
