3de2f658afda6aec52f67bbb12caee6c58d93d72
huangkui <huangkui198707@qq.com>
2020-01-25 23:27:18 +0800

    20200125

--- python/spider/03meiz.py
+++ python/spider/03meiz.py
@@ -19,7 +19,7 @@ class get_photo(object):
         self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
                 'Referer':'https://www.mzitu.com',
                 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'}
-        self.target = 'https://www.mzitu.com/tag/shishen/page/7/'
+        self.target = 'https://www.mzitu.com/tag/shishen/page/8/'
 
 
         self.mmids = []
